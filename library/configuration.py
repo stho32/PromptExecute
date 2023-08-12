8bcf22d52cd8e37bb14818832dfe70cbd376860d
Stefan Hoffmann <stho32github@gmail.com>
2023-08-12 20:19:56 +0200

    rewrite von main und configuration

--- library/configuration.py
+++ library/configuration.py
@@ -1,25 +1,25 @@
-import json
 import os
+import json
 
-CONFIG_FILE = "config.json"
-GPT_4_KEY = "gpt_4_key"
+CONFIG_FILE = 'config.json'
+GPT4_KEY = 'gpt_4_key'
 
 def initialize_config():
     """
-    This function checks if a configuration file already exists. 
-    If not, it prompts the user to enter a GPT-4 key and saves this key in a new configuration file. 
+    Checks if a configuration file already exists. If not, it prompts the user to enter a GPT-4 key and saves this key in a new configuration file.
     If the file already exists, no action is taken.
     """
     if not os.path.exists(CONFIG_FILE):
-        gpt_4_key = input("Please enter your GPT-4 key: ")
+        gpt4_key = input("Please enter your GPT-4 key: ")
+        config = {GPT4_KEY: gpt4_key}
         with open(CONFIG_FILE, 'w') as f:
-            json.dump({GPT_4_KEY: gpt_4_key}, f)
-        print("Configuration file created. Please restart the application.")
+            json.dump(config, f)
+        print(f"Configuration saved in {CONFIG_FILE}. Please restart the application.")
         exit()
 
 def load_config():
     """
-    This function reads the configuration file and returns its content as a dictionary.
+    Reads the configuration file and returns its content as a dictionary.
     """
     with open(CONFIG_FILE, 'r') as f:
         config = json.load(f)
@@ -27,8 +27,7 @@ def load_config():
 
 def get_gpt4_key():
     """
-    This function retrieves the GPT-4 key from the configuration file. 
-    It uses the load_config method to read the file and returns the key as a string.
+    Retrieves the GPT-4 key from the configuration file. It uses the load_config() method to read the file and returns the key as a string.
     """
     config = load_config()
-    return config[GPT_4_KEY]
+    return config[GPT4_KEY]
--- main.py
+++ main.py
@@ -1,109 +1,84 @@
-
 import os
-import sys
-import json
 import hashlib
-from library.chatgpt import call_gpt_4
-from library.interface_creator import extract_functions
+from library import configuration, chatgpt
 
-CONFIG_FILE = "config.json"
-GPT4_KEY = "gpt_4_key"
-PROMPT_EXT = ".prompt"
-CHECKSUM_EXT = ".prompt.checksum"
-COMPLETED_EXT = ".prompt.completed"
-OUTPUT_EXT = ".prompt.output"
-INTERFACE_EXT = ".interface"
+def calculate_checksum(file_path):
+    """
+    Calculates the MD5 checksum of a file.
+    """
+    hasher = hashlib.md5()
+    with open(file_path, 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    return hasher.hexdigest()
 
-def get_gpt4_key():
-    if os.path.exists(CONFIG_FILE):
-        with open(CONFIG_FILE) as f:
-            config = json.load(f)
-            return config.get(GPT4_KEY)
-    else:
-        key = input("Please enter your GPT-4 key: ")
-        with open(CONFIG_FILE, 'w') as f:
-            json.dump({GPT4_KEY: key}, f)
-        print(f"Config file {CONFIG_FILE} has been created.")
-        sys.exit(0)
+def process_file(file_path):
+    """
+    Processes a .prompt file, preparing the prompt, sending it to chat-GPT4, and handling the output.
+    """
+    print(f"Processing file: {file_path}")
+    prepared_prompt = []
+    output_file_name = None
 
-def process_prompt_file(filepath, gpt4_key):
-    print(f"Processing file: {filepath}")
-    with open(filepath) as f:
-        lines = f.readlines()
+    with open(file_path, 'r') as file:
+        for line in file:
+            if line.startswith('#include'):
+                include_file_path = os.path.join(os.path.dirname(file_path), line[8:].strip())
+                with open(include_file_path, 'r') as include_file:
+                    prepared_prompt.append(include_file.read())
+            elif line.startswith('#write-to'):
+                output_file_name = os.path.join(os.path.dirname(file_path), line[9:].strip())
+            else:
+                prepared_prompt.append(line)
 
-    checksum = hashlib.md5(''.join(lines).encode()).hexdigest()
-    checksum_file = filepath + CHECKSUM_EXT
+    prepared_prompt_str = '\n'.join(prepared_prompt)
+    with open(file_path + '.completed', 'w') as completed_file:
+        completed_file.write(prepared_prompt_str)
 
-    if os.path.exists(checksum_file):
-        with open(checksum_file) as f:
-            old_checksum = f.read().strip()
-        if old_checksum == checksum:
-            print(f"No changes in {filepath}. Skipping.")
-            return
-    with open(checksum_file, 'w') as f:
-        f.write(checksum)
+    print("Sending prompt to chat-GPT4...")
+    gpt4_key = configuration.get_gpt4_key()
+    gpt4_output = chatgpt.call_gpt_4(gpt4_key, prepared_prompt_str)
 
-    prepared_prompt = ""
-    output_file_name = ""
-    create_interface = False
-
-    for line in lines:
-        if line.startswith("#include"):
-            include_file = os.path.join(os.path.dirname(filepath), line.split()[1].strip())
-            with open(include_file) as f:
-                prepared_prompt += f.read()
-        elif line.startswith("#write-to"):
-            output_file_name = os.path.join(os.path.dirname(filepath), line.split()[1].strip())
-        elif line.startswith("#create-interface"):
-            create_interface = True
-        else:
-            prepared_prompt += line
-
-    with open(filepath + COMPLETED_EXT, 'w') as f:
-        f.write(prepared_prompt)
-
-    gpt4_output = call_gpt_4(gpt4_key, prepared_prompt)
-
-    with open(filepath + OUTPUT_EXT, 'w') as f:
-        f.write(gpt4_output)
+    with open(file_path + '.output', 'w') as output_file:
+        output_file.write(gpt4_output)
 
     if output_file_name:
-        post_process(gpt4_output, output_file_name, create_interface)
+        post_process_output(output_file_name, gpt4_output)
 
-def post_process(gpt4_output, output_file_name, create_interface):
-    print(f"Post processing for: {output_file_name}")
-    lines = gpt4_output.split('\n')
-    code_lines = []
+def post_process_output(output_file_name, gpt4_output):
+    """
+    Post-processes the output of chat-GPT4, extracting code blocks and writing them to a file.
+    """
+    print(f"Post-processing output to: {output_file_name}")
+    code_blocks = gpt4_output.split('```')[1::2]
+    with open(output_file_name, 'w') as output_file:
+        output_file.write('\n'.join(code_blocks))
 
-    start = False
-    for line in lines:
-        if line.strip() == "```":
-            start = not start
-            continue
-        if start:
-            code_lines.append(line)
-
-    with open(output_file_name, 'w') as f:
-        f.write('\n'.join(code_lines))
-
-    if create_interface:
-        interface_content = extract_functions('\n'.join(code_lines), output_file_name)
-        with open(output_file_name + INTERFACE_EXT, 'w') as f:
-            f.write(interface_content)
-
-def walk_directory(directory, gpt4_key):
+def main(directory):
+    """
+    Walks through a directory structure, looking for .prompt files and processing them.
+    """
+    print(f"Starting in directory: {directory}")
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith(PROMPT_EXT):
-                process_prompt_file(os.path.join(root, file), gpt4_key)
+            if file.endswith('.prompt'):
+                file_path = os.path.join(root, file)
+                checksum_file_path = file_path + '.checksum'
+                current_checksum = calculate_checksum(file_path)
 
-def main():
-    directory = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
-    gpt4_key = get_gpt4_key()
-    if not gpt4_key:
-        print("GPT-4 key is not found in the config file.")
-        sys.exit(1)
-    walk_directory(directory, gpt4_key)
+                if os.path.exists(checksum_file_path):
+                    with open(checksum_file_path, 'r') as checksum_file:
+                        previous_checksum = checksum_file.read().strip()
+                else:
+                    previous_checksum = None
+
+                if current_checksum != previous_checksum:
+                    with open(checksum_file_path, 'w') as checksum_file:
+                        checksum_file.write(current_checksum)
+                    process_file(file_path)
 
 if __name__ == "__main__":
-    main()
+    import sys
+    directory = sys.argv[1] if len(sys.argv) > 1 else '.'
+    configuration.initialize_config()
+    main(directory)
